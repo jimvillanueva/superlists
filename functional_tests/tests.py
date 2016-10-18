3f8fad15b5245c5e092db88b73066582a4c0e640
Jim Villanueva <jim.villanueva@hotmail.com>
2016-10-18 18:25:26 -0400

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -90,6 +90,4 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
         
         # Satisfied, they both go back to sleep
-        self.fail('Finish the test!')
-
-#    unittest.main(warnings='ignore')
+        
