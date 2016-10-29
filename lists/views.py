94481f9d42c6701803620fb3d39d817a047b97dd
Jim Villanueva <jim.villanueva@hotmail.com>
2016-10-29 00:51:53 -0400

    rename all the input ids and names. still broken (10/29/16)

--- lists/views.py
+++ lists/views.py
@@ -11,7 +11,7 @@ def home_page(request):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -29,11 +29,14 @@ def view_list(request, list_id):
     if request.method == 'POST':
         try:
             
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
+            # next statement added from the top of pg. 205
+            # causes error "Template 'list.html' was not a template used to render the response." in UT's
+            return render(request, 'home.html', {"error": error})
             
     return render(request, 'list.html', {'list': list_, 'error': error})
